381c98547e4ee7b9f2991c4d14a9e26c085eb7b0
Leticia <you@example.com>
2025-11-21 19:29:25 -0300

    NinjaModel

---
+++ src/main/java/dev/java10xx/NinjaCadastro/NinjaCadastroApplication.java
@@ -0,0 +1,13 @@
+package dev.java10xx.NinjaCadastro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NinjaCadastroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NinjaCadastroApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/java10xx/NinjaCadastro/NinjaCadastroApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.java10xx.NinjaCadastro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NinjaCadastroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
